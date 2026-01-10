0b6853227520195775e5158690a8c743908dd026
Freddy <frederick@theezerkenegdo.com>
2026-01-10 02:06:20 -0600

    Update header.php

--- header.php
+++ header.php
@@ -113,7 +113,7 @@
 
                         <!-- Desktop Contact Card (Moved) -->
                         <div class="header-contact-card">
-                            <a href="tel:+17202207105" class="contact-link">
+                            <a href="tel:+17206989170" class="contact-link">
                                 <svg class="contact-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round">
@@ -121,7 +121,7 @@
                                         d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
                                     </path>
                                 </svg>
-                                (720) 220-7105
+                                (720) 698-9170
                             </a>
                             <a href="mailto:ceo@florenceinternational.health" class="contact-link">
                                 <svg class="contact-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14"
